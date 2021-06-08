7a661ecd6c56e2deff97931b5a9e939f0fcd70f0
Msyoka <daviemumo37@gmail.com>
2021-06-08 13:05:03 +0300

    change db settings

--- neybuzz/settings.py
+++ neybuzz/settings.py
@@ -96,12 +96,16 @@ WSGI_APPLICATION = 'neybuzz.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME', 'postgres'),
+        'USER': config('DB_USER', 'postgres'),
+        'PASSWORD': config('DB_PASSWORD', ''),
+        'HOST': config('DB_HOST', 'localhost'),
+        'PORT': '',
+       
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
